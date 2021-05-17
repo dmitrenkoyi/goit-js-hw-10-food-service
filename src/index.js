3cb3bc0a545777b1a88cfc7cd7560a6ba1d6d3f9
dmitrenkoyi <artemovayi@gmail.com>
2021-05-17 21:48:43 +0300

    исправила подключение стилей

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import menuCardTpl from './templates/template-menu.handlebars';
 import menu from './menu.json';
 import { markupMenuCard } from './js/rander-menu.js';
 import { changeTheme } from './js/change-theme.js';
+import './sass/main.scss';
 
 markupMenuCard(menu, menuCardTpl);
 changeTheme();
